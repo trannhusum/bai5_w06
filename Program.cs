8dbd7d0b9d259443b40d7a3b1cbbab93c1740408
trannhusum <147652638+trannhusum@users.noreply.github.com>
2024-03-07 17:09:30 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai5_w06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rd = new Random();
+            int[] arr = new int[100000];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = (int)rd.Next(500);
+            }
+
+            StopWatch sw = new StopWatch();
+            sw.Start();
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int minIndex = i;
+                int minCurrent = arr[i];
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (minCurrent > arr[j])
+                    {
+                        minCurrent = arr[j];
+                        minIndex = j;
+                    }
+                }
+                int temp = arr[minIndex];
+                arr[minIndex] = arr[i];
+                arr[i] = temp;
+            }
+            sw.Stop();
+            Console.WriteLine("Thoi gian chay: " + sw.GetElapsedTime());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StopWatch.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace bai5_w06
+{
+    public class StopWatch
+    {
+        private DateTime startTime;
+        private DateTime endTime;
+
+        public DateTime StartTime => startTime;
+        public DateTime EndTime => endTime;
+
+        public StopWatch()
+        {
+            startTime = DateTime.Now;
+        }
+
+        public void Start()
+        {
+            startTime = DateTime.Now;
+        }
+
+        public void Stop()
+        {
+            endTime = DateTime.Now;
+        }
+
+        public double GetElapsedTime()
+        {
+            return (endTime - startTime).TotalMilliseconds;
+        }
+
+        public DateTime GetStartTime()
+        {
+            return startTime;
+        }
+
+        public DateTime GetEndTime()
+        {
+            return endTime;
+        }
+    }
+}
